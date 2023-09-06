3003776906742c5ce8038d3335252b0543e6e413
JeffErasLindao <jeras@espol.edu.ec>
2023-09-06 06:52:26 -0500

    Se actualizo Swagger

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -17,6 +17,100 @@ const getUsuarios = function (req, res, next) {
       res.json(resultado);
     })
     .catch((error) => res.status(400).send(error));
+
+
+    
 };
 
+const getUsuariosById = function (req, res, next) {
+
+  let id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+      res.status(400).json({ error: "El identificador de estudiante no es válido." });
+      return;
+  }
+    usuario.findByPk(id)
+        .then(instancia => {
+            if (instancia) {
+                res.status(200).json(instancia);
+            } else {
+                res.status(404).json({ error: "No existe registro con el identificador " + id })
+            }
+        })
+        .catch(error => res.status(400).send(error))
+    
+};
+
+
+const putUsuarios = function (req, res, next) {
+  usuario.create(req.body)
+  .then(instancia => {
+    res.status(201).json(instancia);
+  })
+  .catch(error => {
+    res.status(500).json({ error: 'Error al crear el registro' });
+  });
+
+    
+};
+
+const updateUsuarios = function (req, res, next) {
+  let id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        res.status(400).json({ error: "El identificador de usuario no es válido." });
+        return;
+    }
+
+    usuario.findByPk(id)
+      .then(instancia => {
+        if(instancia) {
+
+          instancia.update(req.body)
+            .then(instanciaActualizada => {
+              res.status(201).json(instanciaActualizada);
+            })
+            .catch(error => {
+              res.status(500).json({ error: 'Error al actualizar el registro' });
+            });
+
+        } else {
+          res.status(404).json({error: "No existe registro con el identificador "+id})
+        }
+      })
+      .catch(error => res.status(400).send(error))
+
+    
+};
+
+const deleteUsuarios = function (req, res, next) {
+   
+    const id = req.params.idEstudiante;
+  
+    usuario.findByPk(id)
+      .then(usuario => {
+        if (usuario) {
+          usuario.destroy()
+            .then(() => {
+              res.status(204).send();
+            })
+            .catch(error => {
+              res.status(500).json({ error: 'Error al eliminar el usuario' });
+            });
+        } else {
+          res.status(404).json({ error: 'usuario no encontrado' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'Error al obtener el usuario' });
+      });
+    
+};
+
+
 module.exports = getUsuarios;
+module.exports = getUsuariosById;
+module.exports = putUsuarios;
+module.exports = updateUsuarios;
+module.exports = deleteUsuarios;
--- routes/rest_usuario.js
+++ routes/rest_usuario.js
@@ -1,8 +1,16 @@
 var express = require('express');
 const getUsuarios  = require('../controllers/usuario.controller');
+const getUsuariosById  = require('../controllers/usuario.controller');
+const putUsuarios  = require('../controllers/usuario.controller');
+const updateUsuarios  = require('../controllers/usuario.controller');
+const deleteUsuarios  = require('../controllers/usuario.controller');
 var router = express.Router();
 
 
 router.get('/findAll/json', getUsuarios);
+router.get('/findById/:idUsuario/json', getUsuariosById);
+router.post('/save', putUsuarios);
+router.put('/update/:idUsuario', updateUsuarios);
+router.delete('/delete/:idUsuario', deleteUsuarios);
   
 module.exports = router;
